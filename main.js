acc62079b77538d0253b5bc446c35606de699b19
Tal Shorer <tal.shorer@gmail.com>
2017-12-22 17:41:05 +0200

    move creeps loop to room to later allow the room to make decisions

--- main.js
+++ main.js
@@ -6,53 +6,45 @@ var getStateModule = function(memory) {
 	return require("state." + memory.state);
 }
 
-var doSpawn = function(spawn, descs) {
+var doSpawn = function(spawn, descs, prefix) {
+	if (spawn.spawning) {
+		var creep = Game.creeps[spawn.spawning.name];
+		spawn.room.visual.text(
+			"🛠️" + creep.memory.role,
+			spawn.pos.x + 1,
+			spawn.pos.y,
+			{align: "left", opacity: 0.8});
+		return;
+	}
 	for (var idx in descs) {
 		desc = descs[idx];
 		var role = desc[0];
-		if (ref.count(role) < desc[1]) {
-			var name = role + Game.time;
+		var refName = prefix + role;
+		if (ref.count(refName) < desc[1]) {
+			var name = spawn.name + "_" + role + "_" + Game.time;
 			if (spawn.spawnCreep(desc[2], name, {memory: {
 				role: role,
+				state: "spawn",
+				refPrefix: prefix,
 			}}) == OK) {
 				console.log("Spawning new creep: " + name);
-				var creep = Game.creeps[name];
-				creep.memory.state = "spawn";
-				ref.get(role);
+				ref.get(refName);
 				return;
 			}
 		}
 	}
 }
 
-module.exports.loop = function () {
-	for (var name in Memory.creeps) {
-		if (!Game.creeps[name]) {
-			var memory = Memory.creeps[name];
-			getStateModule(memory).cleanup(memory);
-			ref.put(memory.role);
-			delete Memory.creeps[name];
-		}
-	}
-
-	var the_spawn = Game.spawns["Spawn1"];
+var doRoom = function(room) {
+	var spawns = room.find(FIND_MY_SPAWNS);
+	var the_spawn = spawns[0];
 
 	doSpawn(the_spawn, [
 		["worker", 4, [WORK, CARRY, MOVE]],
 		["upgrader", 1, [WORK, CARRY, MOVE]],
-	]);
+	], room.name);
 
-	if (the_spawn.spawning) {
-		var creep = Game.creeps[the_spawn.spawning.name];
-		the_spawn.room.visual.text(
-			"🛠️" + creep.memory.role,
-			the_spawn.pos.x + 1,
-			the_spawn.pos.y,
-			{align: "left", opacity: 0.8});
-	}
-
-	for (var name in Game.creeps) {
-		var creep = Game.creeps[name];
+	for (var creep of room.find(FIND_MY_CREEPS)) {
 		if (getStateModule(creep.memory).run(creep)) {
 			getStateModule(creep.memory).cleanup(creep.memory);
 			while (true) {
@@ -66,5 +58,29 @@ module.exports.loop = function () {
 			}
 		}
 		creep.say(creep.memory.state);
+	}
+}
+
+module.exports.loop = function () {
+	for (var name in Memory.creeps) {
+		if (!Game.creeps[name]) {
+			var memory = Memory.creeps[name];
+			getStateModule(memory).cleanup(memory);
+			ref.put(memory.refPrefix + memory.role);
+			delete Memory.creeps[name];
+		}
+	}
+
+	if (Memory.rooms === undefined) {
+		var the_room;
+		for (var name in Game.spawns) {
+			the_room = Game.spawns[name].room;
+			break;
+		}
+		the_room.memory = {};
+	}
+
+	for (var name in Memory.rooms) {
+		doRoom(Game.rooms[name]);
 	}
 }
