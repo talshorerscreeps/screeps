e549ace920c615d3a8bb0d373932f8616cdd0e9c
Tal Shorer <tal.shorer@gmail.com>
2017-12-24 22:19:30 +0200

    rename state.charge to state.deposit

--- role.worker.js
+++ role.worker.js
@@ -7,8 +7,8 @@ module.exports.next = function(creep) {
   if (creep.carry.energy == 0)
     return "harvest";
   else if (creep.memory.state == "harvest")
-    return "charge";
-  else if (creep.memory.state == "charge")
+    return "deposit";
+  else if (creep.memory.state == "deposit")
     return "build";
   else if (creep.memory.state == "build")
     return "upgrade";
